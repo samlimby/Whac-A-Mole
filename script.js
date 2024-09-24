c416c59da94dc60798fa05bc9f2fb6f3c85769c3
samlimby <129619263+samlimby@users.noreply.github.com>
2024-09-24 22:25:36 +0100

    moles initalized

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const firstMole = document.getElementById("mole1");
+const secondMole = document.getElementById("mole2");
+const thirdMole = document.getElementById("mole3");
+const fourthMole = document.getElementById("mole4");
+const fifthMole = document.getElementById("mole5");
+const sixthMole = document.getElementById("mole6");
+
+firstMole.addEventListener("click", function(){
+    console.log("first mole has been clicked - beware!");
+});
+
+secondMole.addEventListener("click", function(){
+    console.log("second mole has been clicked - beware!");
+});
+
+thirdMole.addEventListener("click", function(){
+    console.log("third mole has been clicked - beware!");
+});
+
+fourthMole.addEventListener("click", function(){
+    console.log("fourth mole has been clicked - beware!");
+});
+
+fifthMole.addEventListener("click", function(){
+    console.log("fifth mole has been clicked - beware!");
+});
+
+sixthMole.addEventListener("click", function(){
+    console.log("sixth mole has been clicked - beware!");
+});
